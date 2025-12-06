ee2c751704c13e713b42ba7c5a4eb6fd96ce3736
Antonio Gonzalez <lsteamer@gmail.com>
2025-12-06 23:22:45 +0100

    Data fetched, list present, images shown.

---
+++ app/index.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  image: string;
+}
+
+export default function Index() {
+
+  const[pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+      const data = await response.json();
+
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+          };
+        })
+      );
+
+      setPokemons(detailedPokemons);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.image}</Text>
+          <Image
+            source={{uri: pokemon.image}}
+            style={{width: 100, height: 100}}
+          />
+        </View>
+      )) }
+    </ScrollView>
+  );
+}
